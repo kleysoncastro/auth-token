f104dacd7402702afcb2e8055658a17cc25d43e6
kleyson castro <kleysoncastro99@gamil.com>
2019-09-29 15:08:20 -0300

    config para hash de email nao voltar, e correcao de msg de erro na duplicidade de email

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -9,9 +9,22 @@ const router = express.Router();
 //post na rota
 
 router.post('/register', async (req, res)=>{
+
+    // recupera instancia do email 
+    const {email} = req.body;
+
     try {
-// correcao da linha 
+
+        if(await User.findOne({email})) {
+
+            return res.status(400).send({erro: "Erro, email já cadastrado"})
+        }
+        
+
         const user = await User.create(req.body)
+
+        // essa linha empeque que a hash de senha retorne no post
+        user.password = undefined;
 
         return res.send({user})
     } catch(error){
--- src/models/user.js
+++ src/models/user.js
@@ -30,6 +30,7 @@ const userSchema = new mongoose.Schema({
 userSchema.pre('save', async function(next) {
     const hash = await bcryptjs.hash(this.password, 10);
     this.password = hash;
+    next();
 })
 
 
